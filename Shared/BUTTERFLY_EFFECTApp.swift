6ab26748bdeb1a00bbbff1742fd187fd6cc66265
Marouane Fagri <mfagri@e3r7p13.1337.ma>
2024-07-01 06:50:04 +0100

    first commit

---
+++ Shared/BUTTERFLY_EFFECTApp.swift
@@ -0,0 +1,17 @@
+//
+//  BUTTERFLY_EFFECTApp.swift
+//  Shared
+//
+//  Created by Marouane Fagri on 7/1/24.
+//
+
+import SwiftUI
+
+@main
+struct BUTTERFLY_EFFECTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Marouane Fagri on 7/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack{
+            Color.red.edgesIgnoringSafeArea(edges: Edge.Set)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
