0f4e14fe55ee739fd4a31527185b36d9f5ffebe3
mfagri <marouane.fagri1@gmail.com>
2024-07-04 12:36:13 +0100

    add tab view

--- Shared/HomeView.swift
+++ Shared/HomeView.swift
@@ -1,27 +1,80 @@
 import SwiftUI
+import UIKit
+struct HomeView: View {
+    @State private var selectedTab = 0
+    init() {
+    UITabBar.appearance().backgroundColor =  UIColor(Color.black)
+    UITabBar.appearance().barTintColor = UIColor(Color.black)
+    UITabBar.appearance().tintColor = UIColor.white.withAlphaComponent(0.5)
+    UITabBar.appearance().unselectedItemTintColor = UIColor.white
+    UITabBar.appearance().isTranslucent = false
+    UITabBar.appearance().layer.borderWidth = 0.0
+}
+    var body: some View {
+        NavigationView {
+            GeometryReader { geometry in
+                VStack {
+                    TabView(selection: $selectedTab) {
+                    
+                        HomeTabView()
+                            .tabItem {
+                                Image(systemName: "house.fill")
+                                Text("Home")
+                            }
+                            .tag(0)
 
-
-//home view
-
-struct HomeView: View{
-    var body: some View{
-        NavigationView{
-            VStack{
-                Color.blue.opacity(0.6)
-                    .edgesIgnoringSafeArea(.top)
-                    .frame(height: 0)
-                Text("Home")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .padding()
-                Spacer()
+                        ProfileView()
+                            .tabItem {
+                                Image(systemName: "person")
+                                Text("Profile")
+                            }
+                            .tag(1)
+                        SettingView()
+                            .tabItem {
+                                Image(systemName: "gear")
+                                Text("Setting")
+                            }
+                            .tag(2)
+                    }.foregroundColor(.black)    
+                }
             }
         }
     }
 }
 
+extension Color {
+    init(hex: UInt, alpha: Double = 1) {
+        self.init(
+            .sRGB,
+            red: Double((hex >> 16) & 0xff) / 255,
+            green: Double((hex >> 08) & 0xff) / 255,
+            blue: Double((hex >> 00) & 0xff) / 255,
+            opacity: alpha
+        )
+    }
+}
+
+struct HomeTabView: View {
+    var body: some View {
+        Text("Home View")
+    }
+}
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile View")
+    }
+}
+
+struct SettingView: View {
+    var body: some View {
+        Text("Profile View")
+    }
+}
+
+
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
-        HomeView()
+        ContentView()
     }
 }
